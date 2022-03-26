28ae14055235bd3af827d4ff7ba0b29cad054545
tavish <tapegram@gmail.com>
2022-03-26 12:56:13 -0500

    Styling the select box slightly

--- web/src/components/RelationshipForm/RelationshipForm.tsx
+++ web/src/components/RelationshipForm/RelationshipForm.tsx
@@ -7,7 +7,6 @@ import {
 } from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
-import { Toaster } from '@redwoodjs/web/toast'
 import { QUERY as RelationshipsQuery } from 'src/components/RelationshipsCell'
 
 const CREATE = gql`
@@ -41,39 +40,44 @@ const RelationshipForm = ({ personId, people }) => {
     refetchQueries: [{ query: RelationshipsQuery, variables: { personId } }],
   })
   const onSubmit = (input) => {
-    createRelationship({ variables: {
-      input: {
-        leftId: personId,
-        rightId: input.person
+    createRelationship({
+      variables: {
+        input: {
+          leftId: personId,
+          rightId: input.person
+        }
       }
-     }
     })
   }
 
   return (
     <div>
-      <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
       <h3>Add Relationship</h3>
       <Form onSubmit={onSubmit}>
         <FormError
           error={error}
         />
+        <div className="inline-block relative w-64">
+          <SelectField
+            name="person"
+            validation={{ required: true, valueAsNumber: true }}
+            className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
 
-        <Label name="person" >
-          Person
-        </Label>
-        <SelectField
-          name="person"
-          validation={{ required: true, valueAsNumber: true }}
+            <option value="" disabled selected>Select a person</option>
+            {people.map((person) => (
+              <option key={person.id} value={person.id}>
+                {person.lastName}, {person.firstName}
+              </option>
+
+            ))}
+          </SelectField>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
+          </div>
+        </div>
+        <Submit
+          className="flex-shrink-0 border-transparent border-4 text-teal-500 hover:text-teal-800 text-sm py-1 px-2 rounded"
         >
-          {people.map((person) => (
-            <option key={person.id} value={person.id}>
-              {person.lastName}, {person.firstName}
-            </option>
-          ))}
-        </SelectField>
-
-        <Submit>
           Submit
         </Submit>
       </Form>
