e59c575c92ed4d444eea82bce464ca43adede995
tavish <tapegram@gmail.com>
2022-03-30 11:35:13 -0400

    Add migration to cascade deletes

---
+++ api/db/migrations/20220330153418_cascade_relationship_deletes/migration.sql
@@ -0,0 +1,14 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Relationship" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "leftId" INTEGER NOT NULL,
+    "rightId" INTEGER NOT NULL,
+    CONSTRAINT "Relationship_leftId_fkey" FOREIGN KEY ("leftId") REFERENCES "Person" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Relationship_rightId_fkey" FOREIGN KEY ("rightId") REFERENCES "Person" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Relationship" ("id", "leftId", "rightId") SELECT "id", "leftId", "rightId" FROM "Relationship";
+DROP TABLE "Relationship";
+ALTER TABLE "new_Relationship" RENAME TO "Relationship";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
