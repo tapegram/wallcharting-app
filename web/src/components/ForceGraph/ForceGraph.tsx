c6b64dce5759418aa848a65e86bd30368d5c142b
tavish <tapegram@gmail.com>
2022-03-31 21:32:21 -0400

    Create a panel component and show/hide it

--- web/src/components/ForceGraph/ForceGraph.tsx
+++ web/src/components/ForceGraph/ForceGraph.tsx
@@ -1,47 +1,66 @@
-import { Graph } from "react-d3-graph";
+import { Graph } from 'react-d3-graph'
 import { navigate, routes } from '@redwoodjs/router'
+import { useState } from 'react'
+import ProfilePanel from '../ProfilePanel/ProfilePanel'
 
 const ForceGraph = ({ nodes, edges }) => {
+  const [selectedPerson, setSelectedPerson] = useState(0)
+  const [showPanel, setShowPanel] = useState(false)
   const state = {
     data: {
       nodes: nodes,
-      links: edges.map((edge) => ({ source: edge.leftId, target: edge.rightId })),
+      links: edges.map((edge) => ({
+        source: edge.leftId,
+        target: edge.rightId,
+      })),
     },
     config: {
+      height: 800,
+      width: 800,
+      automaticRearrangeAfterDropNode: true,
+      freezeAllDragEvents: false,
+      staticGraph: false,
+      staticGraphWithDragAndDrop: false,
       collapsible: false,
       nodeHighlightBehavior: true,
       node: {
-        color: "blue",
-        size: 250,
+        color: 'blue',
+        size: 400,
         fontSize: 12,
-        highlightStrokeColor: "blue",
-        labelProperty: "label",
+        highlightStrokeColor: 'blue',
+        labelProperty: 'label',
       },
       link: {
-        highlightColor: "lightblue"
+        highlightColor: 'lightblue',
       },
-      "d3": {
-        "alphaTarget": 0.05,
-        "gravity": -150,
-        "linkLength": 100,
-        "linkStrength": 1,
-        "disableLinkForce": false
+      d3: {
+        alphaTarget: 0.05,
+        gravity: -250,
+        linkLength: 120,
+        linkStrength: 2,
+        disableLinkForce: false,
       },
     },
-    fontSize: 12
-  };
+    fontSize: 12,
+  }
 
-  const onClickNode = nodeId => {
-    navigate(routes.profile({ id: nodeId }))
-  };
+  const onClickNode = (nodeId) => {
+    // navigate(routes.profile({ id: nodeId }))
+    setSelectedPerson(parseInt(nodeId))
+    setShowPanel(true)
+  }
 
-  return <Graph
-    id="ontology-graph"
-    data={state.data}
-    config={state.config}
-    onClickNode={onClickNode}
-  />
+  return (
+    <>
+      <p>{showPanel ? <ProfilePanel id={selectedPerson} /> : <></>}</p>
+      <Graph
+        id="ontology-graph"
+        data={state.data}
+        config={state.config}
+        onClickNode={onClickNode}
+      />
+    </>
+  )
 }
-
 
 export default ForceGraph
